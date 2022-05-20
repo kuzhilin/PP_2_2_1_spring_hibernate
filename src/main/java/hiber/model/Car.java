0741fe2e7a5d174add7e82617981203726b394b9
kuzhilin <kuzhilin94@yandex.ru>
2022-05-20 15:49:19 +0700

    add car

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,51 @@
+package hiber.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String model;
+
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
